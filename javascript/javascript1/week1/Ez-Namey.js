ccc1ba598854cbee3bc02e6dd4096dc8049c7749
simonreddy2001 <77497059+simonreddy2001@users.noreply.github.com>
2021-05-10 18:01:35 +0200

    fixed bugs

--- javascript/javascript1/week1/Ez-Namey.js
+++ javascript/javascript1/week1/Ez-Namey.js
@@ -1,6 +1,30 @@
-const firstWords = ["one", "easy","elated","elegant","embarrassed","enchanting","encouraging","energetic","enthusiastic","envious"]
-const secondWords = ["evil","excited","expensive","exuberant","good","gorgeous","graceful","grieving","grotesque","grumpy"]
-let randomNumber1 = Math.floor(Math.random() * 10)
-let randomNumber2 = Math.floor(Math.random() * 10)
-let startupName = firstWords[randomNumber1]+" "+secondWords[randomNumber2]
+const firstWords = [ 
+            "one", 
+            "easy",
+            "elated",
+            "elegant",
+            "embarrassed",
+            "enchanting",
+            "encouraging",
+            "energetic",
+            "enthusiastic",
+            "envious"
+        ]
+const secondWords = [
+            "evil",
+            "excited",
+            "expensive",
+            "exuberant",
+            "good",
+            "gorgeous",
+            "graceful",
+            "grieving",
+            "grotesque",
+            "grumpy"
+        ]
+
+let randomNumber1 = Math.floor(Math.random() * firstWords.length)
+let randomNumber2 = Math.floor(Math.random() * secondWords.length)
+let startupName = firstWords[randomNumber1] + " " + econdWords[randomNumber2]
+
 console.log(`The startup: "${startupName}" contains ${startupName.length} characters`)
--- javascript/javascript1/week1/Goodboy-Oldboy.js
+++ javascript/javascript1/week1/Goodboy-Oldboy.js
@@ -2,11 +2,12 @@ let dogYearOfBirth = 2017
 let dogYearFuture = 2027
 let dogYear = dogYearFuture-dogYearOfBirth
 let shouldShowResultInDogYears = true
-if(shouldShowResultInDogYears == true){
-    console.log(`Your dog will be ${dogYear} human years old in ${dogYearFuture}`)
+
+if(shouldShowResultInDogYears){
+    console.log(`Your dog will be ${dogYear*7} dog years old in ${dogYearFuture}`) 
 }
 else{
-    console.log(`Your dog will be ${dogYear*7} dog years old in ${dogYearFuture}`)
+    console.log(`Your dog will be ${dogYear} human years old in ${dogYearFuture}`)
 }
     
 
