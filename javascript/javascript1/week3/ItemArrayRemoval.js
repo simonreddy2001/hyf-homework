1f6280f9e16d40bcb3149a19c03cf3900ddb8395
simonreddy2001 <77497059+simonreddy2001@users.noreply.github.com>
2021-05-17 22:44:45 +0200

    update

--- javascript/javascript1/week3/ItemArrayRemoval.js
+++ javascript/javascript1/week3/ItemArrayRemoval.js
@@ -10,6 +10,6 @@ const names = [
   ];
   const nameToRemove = "Ahmad";
   
-  namesnames.splice(names.indexOf(nameToRemove),1)
+  names.splice(names.indexOf(nameToRemove),1)
   
   console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
