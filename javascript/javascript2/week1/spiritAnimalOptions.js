1a8c92c85c674c7d1389a0620fe9705cebd18f1d
simonreddy2001 <77497059+simonreddy2001@users.noreply.github.com>
2021-06-04 18:25:09 +0200

    optional

--- javascript/javascript2/week1/spiritAnimalOptions.js
+++ javascript/javascript2/week1/spiritAnimalOptions.js
@@ -1,46 +1,46 @@
 const spiritNames = [
-    "The Butterfly", 
-    "The Spider",
-    "The Crow",
-    "The Owl",
-    "The Hummingbird",
-    "The Hawk",
-    "The Grasshopper",
-    "The Eagle",
-    "The Lion",
-    "The Tiger"
-    ]
+  "The Butterfly",
+  "The Spider",
+  "The Crow",
+  "The Owl",
+  "The Hummingbird",
+  "The Hawk",
+  "The Grasshopper",
+  "The Eagle",
+  "The Lion",
+  "The Tiger",
+];
 
-function spiritNamesGenerator(name){
-    return `${name} - ${spiritNames[parseInt(Math.random()*10)]}`
+function spiritNamesGenerator(name) {
+  return `${name} - ${spiritNames[parseInt(Math.random() * 10)]}`;
 }
 
-function showAnimalName(){
-    let userName=document.getElementById('inputName').value
-    if(userName!==""){
-        sptName.innerHTML=spiritNamesGenerator(userName)
-    }
-    else{
-        sptName.innerHTML="please provide your name"
-    }
-    btn.disabled = true;
+function showAnimalName() {
+  let userName = document.getElementById("inputName").value;
+  if (userName !== "") {
+    sptName.innerHTML = spiritNamesGenerator(userName);
+  } else {
+    sptName.innerHTML = "please provide your name";
+  }
 }
-const input = document.getElementById('inputName')
-const btn=document.querySelector('.btn1')
+const input = document.getElementById("inputName");
+const btn = document.querySelector(".btn1");
 
-const sptName=document.querySelector('h3')
-
-btn.addEventListener('click',showAnimalName)
-
+const sptName = document.querySelector("h3");
+const userOptionSelect = document.getElementById("userOptions")
 let userChoice = "";
-document.getElementById("userOptions").onclick = function() {
-    userChoice = document.querySelector('input[name = userOption]:checked').value;
-    if(userChoice === "Click the button") {
-        btn.addEventListener("click", showAnimalName);
-        btn.innerHTML="click to generate new Spiral Animal Name"
-    } else if(userChoice === "Hover over the input field") {
-        sptName.addEventListener("mouseover", showAnimalName);
-    } else if(userChoice === "While I type") {
-        sptName.addEventListener("keydown", showAnimalName);
-    } 
-}
+userOptionSelect.addEventListener('click', ()=> {
+  userChoice = document.querySelector("input[name = userOption]:checked").value;
+  sptName.innerHTML = "please provide your name";
+  input.innerText= "";
+  btn.removeEventListener("click", showAnimalName);
+  input.removeEventListener("mouseover", showAnimalName);
+  input.removeEventListener("keydown", showAnimalName);
+  if (userChoice === "Click the button") {
+    btn.addEventListener("click", showAnimalName);
+  } else if (userChoice === "Hover over the input field") {
+    input.addEventListener("mouseover", showAnimalName);
+  } else if (userChoice === "While I type") {
+    input.addEventListener("keydown", showAnimalName);
+  }
+})
