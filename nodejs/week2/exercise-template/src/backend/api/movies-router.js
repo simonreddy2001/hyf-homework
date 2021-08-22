948b9f6d246984b3a10fb51b58dd1c7777518487
simonreddy2001 <simon.reddy2001@gmail.com>
2021-08-22 15:56:22 +0200

    exercise

--- nodejs/week2/exercise-template/src/backend/api/movies-router.js
+++ nodejs/week2/exercise-template/src/backend/api/movies-router.js
@@ -1,11 +1,41 @@
+'use strict'
 const express = require("express");
 const router = express.Router();
 
 const movies = require("../data/movies.json");
 
 router.get("/", async (request, response) => {
-  console.log(movies);
-  response.send({ data: [] });
+  console.log(request.isChromeBrowser)
+  //return response.send(request.isChromeBrowser)
+  //console.log(movies);
+  const query = request.query
+  console.log(request.query)
+
+  if (query.beginYear && query.endYear && query.minRating) {
+    response.send({
+      data:
+        [movies.filter(movie => movie.year >= query.beginYear && movie.year <= query.endYear && movie.rating >= query.minRating)]
+    });
+  }
+  else if (query.beginYear && query.endYear) {
+    response.send({
+      data:
+        [movies.filter(movie => movie.year >= query.beginYear && movie.year <= query.endYear)]
+    });
+  }
+  else {
+    response.send({ data: [movies] });
+  }
 });
+
+router.get("/:id", async (request, response) => {
+  response.send({
+    data:
+      [movies.filter(movie => movie.id == request.params.id)]
+  });
+});
+
+
+
 
 module.exports = router;
