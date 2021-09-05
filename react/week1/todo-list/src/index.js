4b32ae4f6a7c888974108e150ad6e29c501ef30e
simonreddy2001 <simon.reddy2001@gmail.com>
2021-09-05 15:58:01 +0200

    hwupdate

--- react/week1/todo-list/src/index.js
+++ react/week1/todo-list/src/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Todo from './App';
+import todos from './todos';
 
-const Todo = function (props) {
-  return <li>{props.todo}, {props.deadline}</li>
-}
+
 ReactDOM.render(
   <>
     <h1>Todo List</h1>
     <ul>
-      <Todo todo="Get out of bed" deadline={"Wed Sep 13 2017"} />
-      <Todo todo="Brush teeth" deadline={"Thu Sep 14 2017"} />
-      <Todo todo="Eat breakfast" deadline={"Fri Sep 15 2017"} />
+      {todos.map((aTodo) => <Todo todo={aTodo.todo} deadline={aTodo.deadline} />)}
     </ul>
   </>,
   document.getElementById('root')
---
+++ react/week1/todo-list/src/todos.js
@@ -0,0 +1,7 @@
+const todos = [
+    { todo: "Get out of bed", deadline: "Wed Sep 13 2017" },
+    { todo: "Brush teeth", deadline: "Thu Sep 14 2017" },
+    { todo: "Eat breakfast", deadline: "Fri Sep 15 2017" }
+]
+
+export default todos;
