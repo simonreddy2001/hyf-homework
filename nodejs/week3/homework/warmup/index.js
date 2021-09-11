d2d9890daf591ae128de9ad83c5b98cb6ec7f355
simon reddy kommareddy <77497059+simonreddy2001@users.noreply.github.com>
2021-09-11 14:34:37 +0200

    update

--- nodejs/week3/homework/warmup/index.js
+++ nodejs/week3/homework/warmup/index.js
@@ -36,6 +36,11 @@ app.get("/calculator/multiply", (req, res) => {
     const result = keys2.reduce((a, b) => parseInt(a) * parseInt(b))
     res.send(`Multiplication of given parameters is ${result}`)
 })
+app.post("/calculator/subtraction", (req, res) => {
+    const query = req.body
+    const keys = Object.values(query)
+    res.send(`Division of given parameters is ${keys[0] - keys[1]}`)
+})
 app.post("/calculator/multiply", (req, res) => {
     const query = req.body
     const keys = Object.values(query)
