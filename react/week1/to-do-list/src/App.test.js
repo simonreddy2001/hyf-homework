69d1421b0d0d8c7e8dfd76e08f781ee0f9d7be3a
simonreddy2001 <simon.reddy2001@gmail.com>
2021-09-11 15:54:46 +0200

    fixed

--- react/week1/to-do-list/src/App.test.js
+++ react/week1/to-do-list/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import Todo from './Todo';
+import App from './App';
 
 test('renders learn react link', () => {
-  render(<Todo />);
+  render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
