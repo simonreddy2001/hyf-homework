b3c2cd9716e145d02f3716e817ef71098da306ba
simonreddy2001 <simon.reddy2001@gmail.com>
2021-09-12 22:23:19 +0200

    homework

--- react/week2/to-do-list/src/App.js
+++ react/week2/to-do-list/src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import todos from './todos';
 import Todo from './Todo'
 import Header from './Header';
-//import AddTodo from './AddTodo';
 
 const App = function () {
 
@@ -10,13 +9,18 @@ const App = function () {
 
     const newTodo = { id: Math.floor(Math.random() * 100), todo: "Random Text for New Todo" };
 
+    function deleteItem(index) {
+        setstate(todos.splice(index, 1))
+    }
+
     return (
         <div>
             <Header />
+
             <button onClick={() => setstate(todos.push(newTodo))}>Add Todo</button>
 
             <ul>
-                {todos.map((aTodo, i) => <Todo key={i} todo={aTodo.todo} />)}
+                {todos.map((aTodo, i) => <><Todo key={i} todo={aTodo.todo} /><br /><button onClick={() => deleteItem(i)}>Delete</button></>)}
             </ul>
         </div>
     );
--- react/week2/to-do-list/src/Todo.js
+++ react/week2/to-do-list/src/Todo.js
@@ -1,18 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
-import todos from './todos';
+
 
 const Todo = function (props) {
 
   const [checked, setchecked] = useState(false);
-  return <label>
+
+  return <>
     <li key={props.id} style={{ textDecorationLine: checked ? 'line-through' : 'none' }}>{props.todo}</li>
     <input type='checkbox' onClick={() => setchecked(!checked)}></input>
-    <br /><button onClick={() => {
-      //const index = todos.findIndex(props.id);
-      //todos.splice(index, 1)
-    }}>Delete</button></label>
+  </>
 }
 
 export default Todo;
